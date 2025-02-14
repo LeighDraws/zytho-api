cec30cf3957bbbd75ec129d70e89a756e6ccb6e9
Leigh <psychotropes661@gmail.com>
2025-02-14 16:54:01 +0100

    :bug: (fix) fixed cors

--- src/app.ts
+++ src/app.ts
@@ -14,19 +14,13 @@ const app:Application = express();
 // pour recevoir des données en json
 app.use(express.json());
 
-const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5175'], // Remplace par l'URL de ton front
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
-  allowedHeaders: ['Content-Type', 'Authorization'], // En-têtes autorisés
-};
+app.use(cors());
 
-app.use(cors(corsOptions));
-
-app.use("/beers", cors(corsOptions), beersRoutes);
-app.use("/breweries", cors(corsOptions), breweriesRoutes);
-app.use("/users", cors(corsOptions), usersRoutes);
-app.use("/auth", cors(corsOptions), authRoutes);
-app.use("/fave", cors(corsOptions), faveRoutes);
+app.use("/beers", cors(), beersRoutes);
+app.use("/breweries", cors(), breweriesRoutes);
+app.use("/users", cors(), usersRoutes);
+app.use("/auth", cors(), authRoutes);
+app.use("/fave", cors(), faveRoutes);
 
 setupSwagger(app)
 
